bb481b3524e39fc5e10fee44f4856591015bd64a
LucianoMarello <lucianomarello27@gmail.com>
2023-06-19 20:56:07 -0300

    conceptos Material UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Footer } from "./components/layout/footer/Footer";
 import { Navbar } from "./components/layout/navbar/Navbar";
 import { Cart } from "./components/pages/cart/Cart";
 import Home from "./components/pages/home/Home";
+import { ProbandoMui } from "./components/pages/probandoMui/ProbandoMui";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Home />
       <Cart />
+      <ProbandoMui/>
       <Footer />
     </div>
   );
