d03fde03d427ce4b52cfcca207b73ec67edc288b
LucianoMarello <lucianomarello27@gmail.com>
2023-08-28 23:02:00 -0300

    inicio autenticacion

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
+import {
+    getAuth,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    signInWithPopup,
+    GoogleAuthProvider
+} from "firebase/auth"
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_API_KEY,
@@ -12,3 +19,23 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
+const auth = getAuth(app)
+
+//Agregar en el onsubmit del formulario de login y registro
+export const logIn = async ({ email, password }) => {
+    let res = await signInWithEmailAndPassword(auth, email, password)
+    return res
+}
+
+export const signUp = async ({ email, password }) => {
+    let res = await createUserWithEmailAndPassword(auth, email, password)
+    return res
+}
+
+let googleProvider = new GoogleAuthProvider()
+
+//ponerla dentro de otra funcion para poder guardar el resultado de la funcion (res)
+export const loginWithGoogle = async () => {
+    let res = await signInWithPopup(auth, googleProvider)
+    return res
+}
