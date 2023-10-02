d262fdb967e85642f2652dd49c3eebd3bfdbf1c2
LucianoMarello <lucianomarello27@gmail.com>
2023-10-02 15:27:38 -0300

    ideas dashboard

--- src/components/pages/dashboard/DashboardContainer.jsx
+++ src/components/pages/dashboard/DashboardContainer.jsx
@@ -1,9 +1,13 @@
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "../../../firebaseConfig";
 import { products } from "../../../productMock";
+import { useState } from "react";
+import { Box, Grid, TextField } from "@mui/material";
 
 const DashboardContainer = () => {
-  const rellenar = () => {
+  const [prods, setProds] = useState(products);
+
+  const fill = () => {
     products.forEach((product) => {
       let refCollection = collection(db, "products");
       addDoc(refCollection, product);
@@ -13,7 +17,29 @@ const DashboardContainer = () => {
   return (
     <div>
       <h1>DASHBOARD</h1>
-      <button onClick={rellenar}>RELLENAR</button>
+      <Box sx={{ margin: "3rem" }}>
+        <h2>Productos</h2>
+        {prods.map((element, index) => {
+          return (
+            <Grid
+              sx={{ display: "flex", justifyContent: "center" }}
+              item
+              key={index}
+            >
+              <h2>{element.name} - </h2>
+              <Grid sx={{ display: "flex" }}>
+                <h2>- Stock: </h2>
+                <TextField
+                  variant="standard"
+                  sx={{ width: "3rem", marginLeft: "1rem" }}
+                  defaultValue={element.stock}
+                ></TextField>
+              </Grid>
+            </Grid>
+          );
+        })}
+        <button onClick={fill}>RELLENAR</button>
+      </Box>
     </div>
   );
 };
